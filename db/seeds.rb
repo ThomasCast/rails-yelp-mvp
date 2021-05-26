78d2a7046bb5671bfd33ab9ce46c6e5be43ab2bf
Thomas Castaldi <tomas2mars@hotmail.fr>
2021-05-26 15:54:03 +0200

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+bristol = { name: "Le Bristol", address: "Paris", category: "french" }
+pizze =  { name: "Pizze", address: "Aix-en-Provence", category: "italian" }
+frite = { name: "La Friterie", address: "Bruxelles", category: "belgian" }
+sushi_bar = { name: "O sushi", address: "Bordeaux", category: "japanese" }
+ravioli = { name: "La cabane des Raviolis", address: "Marseille", category: "chinese" }
+
+[ bristol, pizze, frite, sushi_bar, ravioli ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
